4e0b5546548b5074fbf324eb4b29ec9405488135
blackjoker14 <86548199+blackjoker14@users.noreply.github.com>
2021-09-19 10:31:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var introduction = "My first console project";
+
+var first_name = "Annan ";
+console.log(first_name);
+
+var last_name = "Ghanto";
+console.log(last_name);
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5; 
+
+var add = a + b ;
+console.log(add);
+
+var subtract = a - b ;
+console.log(subtract);
+
+var multi = a * b ;
+console.log(multi);
+
+var div = a / b ;
+console.log(div);
+
+
+
